77c4c1d81857cd6effadd66da41cdc134c1a6cfa
William Fernandes Dias <william.winchester1967@gmail.com>
2024-03-04 16:34:00 -0300

    chore: standardize model registration

--- src/auth/admin.py
+++ src/auth/admin.py
@@ -4,10 +4,8 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
+@admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ["username", "email", "is_active", "is_staff", "is_superuser"]
     search_fields = ["username", "email"]
     list_filter = ["is_active", "is_staff", "is_superuser"]
-
-
-admin.site.register(User, UserAdmin)

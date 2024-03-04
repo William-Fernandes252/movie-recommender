f0d858f29f3f362ee7edf8d9a5a505589f0476fa
William Fernandes Dias <william.winchester1967@gmail.com>
2024-03-04 16:33:08 -0300

    feat: add id to list display and set some fields to be read only

--- src/movies/admin.py
+++ src/movies/admin.py
@@ -3,11 +3,10 @@ from django.contrib import admin
 from . import models
 
 
+@admin.register(models.Movie)
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ("title", "released")
+    list_display = ("id", "title", "released")
     search_fields = ("title", "overview")
     list_filter = ("released",)
     date_hierarchy = "released"
-
-
-admin.site.register(models.Movie, MovieAdmin)
+    readonly_fields = ("created", "modified", "id")

381fc037cc80e32e3a615f277fa51cd90125a354
William Fernandes Dias <william.winchester1967@gmail.com>
2024-03-06 19:36:44 -0300

    feat: use full text search over the movie titles and overviews

--- src/movies/admin.py
+++ src/movies/admin.py
@@ -7,7 +7,7 @@ from . import models
 @admin.register(models.Movie)
 class MovieAdmin(admin.ModelAdmin):
     list_display = ("id", "title", "released")
-    search_fields = ("title", "overview", "id")
+    search_fields = ("@title", "@overview", "id")
     list_filter = ("released",)
     date_hierarchy = "released"
     readonly_fields = (

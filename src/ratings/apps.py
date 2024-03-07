ca58bb9131511e54bf58126de74b53b936a446b6
William Fernandes Dias <william.winchester1967@gmail.com>
2024-03-06 22:55:38 -0300

    feat: implement signal to deactivate old reviews of a user for a object when a new one is created

--- src/ratings/apps.py
+++ src/ratings/apps.py
@@ -1,6 +1,18 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_save
 
 
 class RatingsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "ratings"
+
+    def ready(self) -> None:
+        super().ready()
+
+        from ratings import models, signals
+
+        post_save.connect(
+            signals.deactivate_old_ratings,
+            sender=models.Rating,
+            dispatch_uid="deactivate_old_ratings",
+        )
---
+++ src/ratings/signals.py
@@ -0,0 +1,24 @@
+from ratings import models
+from ratings.querysets import RatingQuerySet
+
+
+def deactivate_old_ratings(
+    sender: type[models.Rating], instance: models.Rating, created: bool, **kwargs
+):
+    """Deactivate users old ratings for a object when a new one is created."""
+    if created:
+        id = instance.pk
+        if instance.active:
+            old_ratings_queryset: RatingQuerySet = (
+                models.Rating.objects.all()
+                .filter(
+                    user=instance.user,
+                    content_type=instance.content_type,
+                    object_id=instance.object_id,
+                )
+                .exclude(pk=id, active=True)
+            )
+            if old_ratings_queryset.exists():
+                old_ratings_queryset.deactivate()
+                instance.active = True
+                instance.save()

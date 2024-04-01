77e3896ba40710f00e9a81701de7575da6784b6b
William Fernandes Dias <william.winchester1967@gmail.com>
2024-04-01 13:12:31 -0300

    feat: include ID on the user detail admin page

--- src/users/admin.py
+++ src/users/admin.py
@@ -9,3 +9,4 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ["username", "email", "is_active", "is_staff", "is_superuser"]
     search_fields = ["username", "email"]
     list_filter = ["is_active", "is_staff", "is_superuser"]
+    readonly_fields = ["id"]
